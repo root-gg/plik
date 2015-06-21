976269e58a076a4b54c5fe60ccf72e61aa49cd67
Charles-Antoine Mathieu <skatkatt@root.gg>
2015-06-21 15:59:29 +0200

    Upload IP whitelist

--- server/common/context.go
+++ server/common/context.go
@@ -70,11 +70,20 @@ func NewPlikContext(name string, req *http.Request) (ctx *PlikContext) {
 	ctx.Context = rootContext.Context.Fork(name).AutoDetach()
 	ctx.Logger = rootContext.Logger.Copy()
 
-	// TODO X-FORWARDED-FOR
-	remoteAddr := strings.Split(req.RemoteAddr, ":")
-	if len(remoteAddr) > 0 {
-		ctx.Set("RemoteIp", remoteAddr[0])
+	var sourceIP string
+	if Config.SourceIPHeader != "" {
+		// Get source ip from header if behind reverse proxy.
+		sourceIP = req.Header.Get(Config.SourceIPHeader)
+		if sourceIP != "" {
+			ctx.Set("RemoteIP", sourceIP)
+		}
+	} else {
+		remoteAddr := strings.Split(req.RemoteAddr, ":")
+		if len(remoteAddr) > 0 {
+			sourceIP = remoteAddr[0]
+		}
 	}
+	ctx.Set("RemoteIP", sourceIP)
 
 	ctx.UpdateLoggerPrefix("")
 	return
@@ -105,7 +114,7 @@ func (ctx *PlikContext) SetFile(fileName string) *PlikContext {
 // UpdateLoggerPrefix sets a new prefix for the context logger
 func (ctx *PlikContext) UpdateLoggerPrefix(prefix string) {
 	str := ""
-	if ip, ok := ctx.Get("RemoteIp"); ok {
+	if ip, ok := ctx.Get("RemoteIP"); ok {
 		str += fmt.Sprintf("[%s]", ip)
 	}
 	if uploadID, ok := ctx.Get("UploadId"); ok {
