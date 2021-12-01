c195ef2e125ee28072c7c1980a7dbdee1339a8d6
Charles-Antoine Mathieu <ca.mathieu@criteo.com>
2021-12-01 12:22:33 +0100

    File data backend : Do not fail when removing missing file

--- server/data/data.go
+++ server/data/data.go
@@ -7,9 +7,10 @@ import (
 )
 
 // Backend interface describes methods that data backend
-// must implements to be compatible with plik.
+// must implements to be compatible with Plik.
 type Backend interface {
 	AddFile(file *common.File, reader io.Reader) (err error)
 	GetFile(file *common.File) (reader io.ReadCloser, err error)
+	// RemoveFile should not fail if the file is not found
 	RemoveFile(file *common.File) (err error)
 }
