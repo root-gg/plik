d00db095e02ed00a78bf02ccaab984dd8b44d525
Charles-Antoine Mathieu <ca.mathieu@criteo.com>
2024-02-27 19:40:01 +0100

    #486 disable auth button

--- webapp/js/custom.js
+++ webapp/js/custom.js
@@ -1,1 +1,8 @@
-const TITLE = "Plik"
+CONFIG = {
+    // Title of the tab and logo
+    "TITLE": "Plik",
+
+    // Disable the authentication button
+    // Authentication page can still be accessed at https://plik.tld/#/login if enabled
+    "DISABLE_AUTH_BUTTON": false
+}
